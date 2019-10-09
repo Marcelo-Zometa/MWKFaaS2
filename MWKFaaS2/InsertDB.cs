ffaba94c6b07437303050c2774b77ca7d58c8d31
Marcelo Paniagua <marcelo.zometa@hotmail.com>
2019-10-08 18:52:19 -0600

    Add project files.

---
+++ MWKFaaS2/InsertDB.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+
+namespace MWKFaaS2
+{
+    public static class InsertDB
+    {
+        [FunctionName("Test3")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+            string lastName = req.Query["lastName"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+            lastName = lastName ?? data?.lastName;
+
+            // Get the connection string from app settings and use it to create a connection.
+            var str = Environment.GetEnvironmentVariable("sqldb_connection");
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                var text = "INSERT INTO dbo.Person " +
+                        "VALUES ('" + name + "', '" + lastName + "')";
+
+
+
+                using (SqlCommand cmd = new SqlCommand(text, conn))
+                {
+                    // Execute the command and log the # rows affected.
+                    var rows = await cmd.ExecuteNonQueryAsync();
+                    log.LogInformation($"{rows} rows were updated");
+                }
+            }
+
+            //This is not for displaying to the user. Final product must not have it
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name} {lastName}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
---
+++ MWKFaaS2/ReadDB.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+
+namespace MWKFaaS2
+{
+    public static class ReadDB
+    {
+        [FunctionName("ReadDB")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            // Get the connection string from app settings and use it to create a connection.
+            var str = Environment.GetEnvironmentVariable("sqldb_connection");
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                var text = "SELECT * FROM dbo.Person;";//WHERE Name = '" + name +"';";
+
+
+
+                using (SqlCommand cmd = new SqlCommand(text, conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            log.LogInformation(reader["Name"].ToString());
+                            log.LogInformation(reader["LastName"].ToString());
+                        }
+                    }
+                }
+            }
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
